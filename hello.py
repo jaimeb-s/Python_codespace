c58b5c7c393d3afcc3b22ea4a93dbd09c7ee8ef2
jaimeb-s <78188704+jaimeb-s@users.noreply.github.com>
2023-09-16 10:37:42 +0000

    add hello.py codespace

---
+++ hello.py
@@ -0,0 +1,10 @@
+"""Module providing a function printing python version."""
+import sys
+
+def print_python_version():
+    """Function printing python version"""
+    print(sys.version)
+
+print("hello world en un codespace")
+
+print_python_version()
